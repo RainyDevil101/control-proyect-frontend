a99af1fc8ef3dfb830a957a860a48b98b9c8ab5e
Alexis Herrera <aheca96@gmail.com>
2022-08-29 14:55:37 -0400

    29082022

--- src/modules/home/views/Homeview.vue
+++ src/modules/home/views/Homeview.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wrapper-home">
-        
+        <img src="../../../assets/home-background.jpg" alt="No disponible">
     </div>
 </template>
 
@@ -11,6 +11,30 @@
     width: 100%;
     min-height: 100vh;
     min-width: 100vw;
+    display: flex;
+}
+
+img {
+  width: 300px;
+  display: block;
+  margin: auto;
+  border: 1px solid white;
+}
+
+@media (min-width: 768px) {
+
+img {
+  width: 550px;
+}
+
+}
+
+@media (min-width: 992px) {
+
+img {
+  width: 700px;
+}
+
 }
 
 </style>
